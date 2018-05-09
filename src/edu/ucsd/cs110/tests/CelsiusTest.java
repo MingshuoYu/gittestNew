1b779ca1e75030173c0238a119448ea18a0886f8
Mingshuo Yu <miy103@ucsd.edu>
2018-05-09 11:41:35 -0700

    error junit test

--- src/edu/ucsd/cs110/tests/CelsiusTest.java
+++ src/edu/ucsd/cs110/tests/CelsiusTest.java
@@ -33,7 +33,7 @@ public class CelsiusTest extends TestCase{
         // Verify the middle of the formatted string
 
         // (Hint: what is the length of the middle of the string?)
-        assertTrue(string.substring(0, endIndex).equals(beginning));
+        assertTrue(string.substring(1, endIndex).equals(beginning));
     }
 
     public void testCelsiusToCelsius()
